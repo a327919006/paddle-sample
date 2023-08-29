8a18a29c19c542032a4e795b28af302e950b3d4a
chennan <327919006@qq.com>
2023-08-29 23:03:23 +0800

    线性回归

--- 03线性回归-房价预测/train.py
+++ 03线性回归-房价预测/train.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib
 import seaborn as sns
 import warnings
+import paddle
 
 warnings.filterwarnings("ignore")
 
@@ -34,3 +35,172 @@ corr_data = df.corr().iloc[-1]
 corr_data = np.asarray(corr_data).reshape(1, 14)
 ax = sns.heatmap(corr_data, cbar=True, annot=True)
 plt.show()
+
+# 下图展示各属性的取值范围分布：
+sns.boxplot(data=df.iloc[:, 0:13])
+plt.show()
+
+features_max = housing_data.max(axis=0)
+features_min = housing_data.min(axis=0)
+features_avg = housing_data.sum(axis=0) / housing_data.shape[0]
+BATCH_SIZE = 20
+
+
+def feature_norm(input):
+    f_size = input.shape
+    output_features = np.zeros(f_size, np.float32)
+    for batch_id in range(f_size[0]):
+        for index in range(13):
+            output_features[batch_id][index] = (input[batch_id][index] - features_avg[index]) / (
+                    features_max[index] - features_min[index])
+    return output_features
+
+
+# 只对属性进行归一化
+housing_features = feature_norm(housing_data[:, :13])
+# print(feature_trian.shape)
+housing_data = np.c_[housing_features, housing_data[:, -1]].astype(np.float32)
+# print(training_data[0])
+# 归一化后的train_data, 看下各属性的情况
+features_np = np.array([x[:13] for x in housing_data], np.float32)
+labels_np = np.array([x[-1] for x in housing_data], np.float32)
+data_np = np.c_[features_np, labels_np]
+df = pd.DataFrame(data_np, columns=feature_names)
+sns.boxplot(data=df.iloc[:, 0:13])
+plt.show()
+
+# 将训练数据集和测试数据集按照8:2的比例分开
+ratio = 0.8
+offset = int(housing_data.shape[0] * ratio)
+train_data = housing_data[:offset]
+test_data = housing_data[offset:]
+
+
+class Regressor(paddle.nn.Layer):
+    def __init__(self):
+        super().__init__()
+        self.fc = paddle.nn.Linear(13, 1, )
+
+    def forward(self, inputs):
+        pred = self.fc(inputs)
+        return pred
+
+
+train_nums = []
+train_costs = []
+
+
+def draw_train_process(iters, train_costs):
+    plt.title("training cost", fontsize=24)
+    plt.xlabel("iter", fontsize=14)
+    plt.ylabel("cost", fontsize=14)
+    plt.plot(iters, train_costs, color='red', label='training cost')
+    plt.show()
+
+
+import paddle.nn.functional as F
+
+y_preds = []
+labels_list = []
+
+
+def train(model):
+    print('start training ... ')
+    # 开启模型训练模式
+    model.train()
+    EPOCH_NUM = 500
+    train_num = 0
+    optimizer = paddle.optimizer.SGD(learning_rate=0.001, parameters=model.parameters())
+    for epoch_id in range(EPOCH_NUM):
+        # 在每轮迭代开始之前，将训练数据的顺序随机的打乱
+        np.random.shuffle(train_data)
+        # 将训练数据进行拆分，每个batch包含20条数据
+        mini_batches = [train_data[k: k + BATCH_SIZE] for k in range(0, len(train_data), BATCH_SIZE)]
+        for batch_id, data in enumerate(mini_batches):
+            features_np = np.array(data[:, :13], np.float32)
+            labels_np = np.array(data[:, -1:], np.float32)
+            features = paddle.to_tensor(features_np)
+            labels = paddle.to_tensor(labels_np)
+            # 前向计算
+            y_pred = model(features)
+            cost = F.mse_loss(y_pred, label=labels)
+            train_cost = cost.numpy()[0]
+            # 反向传播
+            cost.backward()
+            # 最小化loss，更新参数
+            optimizer.step()
+            # 清除梯度
+            optimizer.clear_grad()
+
+            if batch_id % 30 == 0 and epoch_id % 50 == 0:
+                print("Pass:%d,Cost:%0.5f" % (epoch_id, train_cost))
+
+            train_num = train_num + BATCH_SIZE
+            train_nums.append(train_num)
+            train_costs.append(train_cost)
+
+
+model = Regressor()
+train(model)
+matplotlib.use('TkAgg')
+draw_train_process(train_nums, train_costs)
+
+# 获取预测数据
+INFER_BATCH_SIZE = 100
+
+infer_features_np = np.array([data[:13] for data in test_data]).astype("float32")
+infer_labels_np = np.array([data[-1] for data in test_data]).astype("float32")
+
+infer_features = paddle.to_tensor(infer_features_np)
+infer_labels = paddle.to_tensor(infer_labels_np)
+fetch_list = model(infer_features)
+
+sum_cost = 0
+for i in range(INFER_BATCH_SIZE):
+    infer_result = fetch_list[i][0]
+    ground_truth = infer_labels[i]
+    if i % 10 == 0:
+        print("No.%d: infer result is %.2f,ground truth is %.2f" % (i, infer_result, ground_truth))
+    cost = paddle.pow(infer_result - ground_truth, 2)
+    sum_cost += cost
+mean_loss = sum_cost / INFER_BATCH_SIZE
+print("Mean loss is:", mean_loss.numpy())
+
+
+def plot_pred_ground(pred, ground):
+    plt.figure()
+    plt.title("Predication v.s. Ground truth", fontsize=24)
+    plt.xlabel("ground truth price(unit:$1000)", fontsize=14)
+    plt.ylabel("predict price", fontsize=14)
+    plt.scatter(ground, pred, alpha=0.5)  # scatter:散点图,alpha:"透明度"
+    plt.plot(ground, ground, c='red')
+    plt.show()
+
+
+plot_pred_ground(fetch_list, infer_labels_np)
+
+import paddle
+
+paddle.set_default_dtype("float64")
+
+# step1:用高层API定义数据集，无需进行数据处理等，高层API为你一条龙搞定
+train_dataset = paddle.text.datasets.UCIHousing(mode='train')
+eval_dataset = paddle.text.datasets.UCIHousing(mode='test')
+
+
+# step2:定义模型
+class UCIHousing(paddle.nn.Layer):
+    def __init__(self):
+        super().__init__()
+        self.fc = paddle.nn.Linear(13, 1, None)
+
+    def forward(self, input):
+        pred = self.fc(input)
+        return pred
+
+
+# step3:训练模型
+model = paddle.Model(UCIHousing())
+model.prepare(paddle.optimizer.Adam(parameters=model.parameters()),
+              paddle.nn.MSELoss())
+model.fit(train_dataset, eval_dataset, epochs=5, batch_size=8, verbose=1)
